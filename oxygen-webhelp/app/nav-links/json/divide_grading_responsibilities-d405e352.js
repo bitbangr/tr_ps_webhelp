69a3128fddc37b01c5947112c5d817e89b574cb5
bitbangr <mgj000@hotmail.com>
2025-01-06 23:24:10 -0800

    initial commit for PrepScanner WebHelp output

---
+++ oxygen-webhelp/app/nav-links/json/divide_grading_responsibilities-d405e352.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"divide_grading_responsibilities-d405e352":{"topics":[{"title":"Task: Dividing Grading Responsibilities","href":"topics\/tasks\/task_dividing_grading_responsibilities.html","attributes":{"data-id":"task_dividing_grading_responsibilities"},"menu":{"hasChildren":false},"tocID":"task_dividing_grading_responsibilities-d405e360","topics":[]}]}};
