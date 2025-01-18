60c43f0085e0b81d216722ed56af46a627eb21a2
bitbangr <mgj000@hotmail.com>
2025-01-18 00:39:57 -0800

    01_18_2025_0040

---
+++ oxygen-webhelp/app/nav-links/json/compile_and_communicate_feedback-d531e393.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"compile_and_communicate_feedback-d531e393":{"topics":[{"title":"Task: Providing Feedback to Students","href":"topics\/tasks\/task_providing_feedback_to_students.html","attributes":{"data-id":"task_providing_feedback_to_students"},"menu":{"hasChildren":false},"tocID":"task_providing_feedback_to_students-d531e401","topics":[]}]}};
