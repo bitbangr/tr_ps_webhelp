bd0362807892c823f23c0a02159f3fe6dda8eb5f
bitbangr <mgj000@hotmail.com>
2025-02-08 15:56:36 -0800

    02_08_2025_1556

---
+++ oxygen-webhelp/app/nav-links/json/divide_grading_responsibilities-d532e533.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"divide_grading_responsibilities-d532e533":{"topics":[{"title":"Task: Dividing Grading Responsibilities","href":"topics\/tasks\/task_dividing_grading_responsibilities.html","attributes":{"data-id":"task_dividing_grading_responsibilities"},"menu":{"hasChildren":false},"tocID":"task_dividing_grading_responsibilities-d532e541","topics":[]}]}};
